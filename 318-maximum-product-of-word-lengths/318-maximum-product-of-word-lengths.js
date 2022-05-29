b88dd2b3dac26802e047f057c73b22a9e3a9c111
이시형 <59095793+SH-Lee2@users.noreply.github.com>
2022-05-29 21:20:33 +0900

    Time: 1281 ms (17.91%), Space: 45.4 MB (76.12%) - LeetHub

--- 318-maximum-product-of-word-lengths/318-maximum-product-of-word-lengths.js
+++ 318-maximum-product-of-word-lengths/318-maximum-product-of-word-lengths.js
@@ -23,31 +23,3 @@ function checkUnique (a,b){
     }
     return true
 }
-
-// const areStringsUnique = (a, b) => {
-//   for (let i = 0; i < a.length; i++){
-//     if ( b.includes(a[i]) ){
-//        return false;
-//     }
-//   } 
-//   return true;
-// };
-
-// const maxProduct = function(words) {
-//   let max = 0;
-//   for (let i = 0; i < words.length - 1; i++) {
-//     for (let j = i + 1; j < words.length; j++) {
-//       const a = words[i];
-//       const b = words[j];
-//       let unique = true;
-
-//       // compare wordz
-//       if ( areStringsUnique(words[i], words[j]) ) {
-//         max = Math.max( max, (words[i].length * words[j].length) )
-//       }
-      
-//     }
-//   }
-
-//   return max;
-// };  
