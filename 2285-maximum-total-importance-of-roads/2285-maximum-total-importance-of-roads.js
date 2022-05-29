5acfe5c4729d82ea7630921c9204d4843cca8030
이시형 <59095793+SH-Lee2@users.noreply.github.com>
2022-05-29 16:06:27 +0900

    Time: 708 ms (100.00%), Space: 124.1 MB (100.00%) - LeetHub

--- 2285-maximum-total-importance-of-roads/2285-maximum-total-importance-of-roads.js
+++ 2285-maximum-total-importance-of-roads/2285-maximum-total-importance-of-roads.js
@@ -1,22 +1,32 @@
+/**
+ * @param {number} n
+ * @param {number[][]} roads
+ * @return {number}
+ */
 var maximumImportance = function(n, roads) {
-    const degree = new Array(n).fill(0);
-    
-    let result = 0;
-    
-    for(let [i, j] of roads){
-        degree[i]++;
-        degree[j]++;
+//     간선이 많은 도시에 더 높은 점수를 준다
+    const adjacencyList = {}
+    for(const road of roads){
+        const [x1,x2] = road
+        if(!adjacencyList[x1]) adjacencyList[x1] = []
+        if(!adjacencyList[x2]) adjacencyList[x2] = []
+        adjacencyList[x1].push(x2)
+        adjacencyList[x2].push(x1)
     }
-	
-    degree.sort((a,b)=>a-b);
-	
-
-     for (let i = 0; i < n; ++i)
-	 
-		 /* i+1 with least possible number (i) because it holds least possible rank that (means value of i with 
-		 degree[i]) gradually increments as rank goes by - as simple as that */
-        result += degree[i] * (i + 1);
-		
-    return result;
-    
+    let arr = [] 
+    for(const [key,value] of Object.entries(adjacencyList)){
+        arr.push([key,value.length])
+    }
+    arr.sort((a,b)=>b[1] - a[1])
+    const valList = {}
+    for(const key of arr){
+        valList[key[0]] = n--
+    }
+    let sum = 0 
+    for(const road of roads){
+        const [x1,x2] = road
+        sum += valList[x1]
+        sum += valList[x2]
+    }
+    return sum
 };
