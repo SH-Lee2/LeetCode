de921796231b1096654fef9d486677f93a777eb8
이시형 <59095793+SH-Lee2@users.noreply.github.com>
2022-05-31 22:00:40 +0900

    Time: 497 ms (33.33%), Space: 66.5 MB (27.78%) - LeetHub

--- 1461-check-if-a-string-contains-all-binary-codes-of-size-k/1461-check-if-a-string-contains-all-binary-codes-of-size-k.js
+++ 1461-check-if-a-string-contains-all-binary-codes-of-size-k/1461-check-if-a-string-contains-all-binary-codes-of-size-k.js
@@ -2,7 +2,7 @@ var hasAllCodes = function(s, k) {
     const binaryCodes = new Set();
     
     for (let i = 0; i<= s.length-k; i++) {
-        binaryCodes.add(s.slice(i,i+k));
+        binaryCodes.add(s.substring(i,i+k));
     }
     return binaryCodes.size === Math.pow(2, k);
 };
