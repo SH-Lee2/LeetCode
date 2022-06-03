ff164924145e7eab0ff3819a239aa202d6bd755a
이시형 <59095793+SH-Lee2@users.noreply.github.com>
2022-06-03 13:51:25 +0900

    Time: 6410 ms (5.33%), Space: 67.6 MB (75.00%) - LeetHub

--- 304-range-sum-query-2d-immutable/304-range-sum-query-2d-immutable.js
+++ 304-range-sum-query-2d-immutable/304-range-sum-query-2d-immutable.js
@@ -13,14 +13,12 @@ var NumMatrix = function(matrix) {
  * @return {number}
  */
 NumMatrix.prototype.sumRegion = function(row1, col1, row2, col2) {
-    const startcol = col1
-    const endcol = col2 
     let sum = 0 
     for(let row=row1; row<=row2; row++){
         for(let col=col1; col<=col2; col++){
             sum += this.matrix[row][col]
         }
-    }    
+    }  
     return sum
 };
 
