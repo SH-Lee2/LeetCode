9ba96102c9879a2c52ad04cfabbe9f0e8e0e4ddc
이시형 <59095793+SH-Lee2@users.noreply.github.com>
2022-06-03 18:11:28 +0900

    Time: 640 ms (44.67%), Space: 84.1 MB (41.80%) - LeetHub

--- 304-range-sum-query-2d-immutable/304-range-sum-query-2d-immutable.js
+++ 304-range-sum-query-2d-immutable/304-range-sum-query-2d-immutable.js
@@ -1,13 +1,33 @@
-class NumMatrix {
-    constructor(M) {
-        let ylen = M.length + 1, xlen = M[0].length + 1
-        this.dp = Array.from({length: ylen}, () => new Array(xlen).fill(0))
-        for (let i = 1; i < ylen; i++)
-            for (let j = 1; j < xlen; j++)
-                this.dp[i][j] = M[i-1][j-1] + this.dp[i-1][j] + this.dp[i][j-1] - this.dp[i-1][j-1]
-    }
-    
-    sumRegion(R1, C1, R2, C2) {
-        return this.dp[R2+1][C2+1] - this.dp[R2+1][C1] - this.dp[R1][C2+1] + this.dp[R1][C1]
-    }
+/**
+ * @param {number[][]} matrix
+ */
+var NumMatrix = function(matrix) {
+    if (matrix.length == 0 || matrix[0].length == 0) return;
+    this.dp = Array.from(Array(matrix.length + 1),()=>Array(matrix[0].length +1).fill(0))
+    for (let r = 0; r < matrix.length; r++) {
+        for (let c = 0; c < matrix[0].length; c++) {
+            this.dp[r][c + 1] = this.dp[r][c] + matrix[r][c];
+        }
+    } 
 };
+
+/** 
+ * @param {number} row1 
+ * @param {number} col1 
+ * @param {number} row2 
+ * @param {number} col2
+ * @return {number}
+ */
+NumMatrix.prototype.sumRegion = function(row1, col1, row2, col2) {
+   let sum = 0 
+   for(let row = row1; row <= row2; row++){
+       sum += this.dp[row][col2+1] - this.dp[row][col1]
+   }
+    return sum
+};
+
+/** 
+ * Your NumMatrix object will be instantiated and called as such:
+ * var obj = new NumMatrix(matrix)
+ * var param_1 = obj.sumRegion(row1,col1,row2,col2)
+ */
