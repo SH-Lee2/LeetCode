a2ba8bbba5fc95cba86b7b00b92472a19770b0a2
이시형 <59095793+SH-Lee2@users.noreply.github.com>
2022-06-05 14:31:53 +0900

    Update 2295-replace-elements-in-an-array.js

--- 2295-replace-elements-in-an-array/2295-replace-elements-in-an-array.js
+++ 2295-replace-elements-in-an-array/2295-replace-elements-in-an-array.js
@@ -11,30 +11,11 @@ var arrayChange = function(nums, operations) {
    for(let i=0; i<operations.length; i++){
        const idx = list[operations[i][0]] === undefined ?   -1 : list[operations[i][0]]
        if(idx >= 0 ){
-           nums[list[operations[i][0]]] = operations[i][1]
+           nums[idx] = operations[i][1]
+           delete list[operations[i][0]]
            list[operations[i][1]] = idx
-       } 
+       }
    }
+   
     return nums
 };
-
-// /**
-//  * @param {number[]} nums
-//  * @param {number[][]} operations
-//  * @return {number[]}
-//  */
-// var arrayChange = function(nums, operations) {
-//     let map = new Map();
-//     for(let i = 0; i < nums.length; i++){
-//         map.set(nums[i], i);
-//     }
-    
-//     let i = 0;
-//     while(i < operations.length){
-//         nums[map.get(operations[i][0])]  = operations[i][1]
-//         map.set(operations[i][1], map.get(operations[i][0]))
-//         i++;
-//     }
-    
-//     return nums
-// };
