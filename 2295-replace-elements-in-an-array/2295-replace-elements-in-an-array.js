5d16ff2d86560680b17a683bd873292757f38fc4
이시형 <59095793+SH-Lee2@users.noreply.github.com>
2022-06-05 13:27:31 +0900

    Time: 467 ms (100.00%), Space: 109.5 MB (100.00%) - LeetHub

--- 2295-replace-elements-in-an-array/2295-replace-elements-in-an-array.js
+++ 2295-replace-elements-in-an-array/2295-replace-elements-in-an-array.js
@@ -4,17 +4,38 @@
  * @return {number[]}
  */
 var arrayChange = function(nums, operations) {
-    let map = new Map();
-    for(let i = 0; i < nums.length; i++){
-        map.set(nums[i], i);
+    const list = {}
+    for(let i=0; i<nums.length;i++){
+        list[nums[i]] = i
     }
-    
-    let i = 0;
-    while(i < operations.length){
-        nums[map.get(operations[i][0])]  = operations[i][1]
-        map.set(operations[i][1], map.get(operations[i][0]))
-        i++;
-    }
-    
+   for(let i=0; i<operations.length; i++){
+       // const idx = list[operations[i][0]] === undefined ?   -1 : list[operations[i][0]]
+       // if(idx >= 0 ){
+           nums[list[operations[i][0]]] = operations[i][1]
+           // delete list[operations[i][0]]
+           list[operations[i][1]] = list[operations[i][0]]
+       // } 
+   }
     return nums
 };
+
+// /**
+//  * @param {number[]} nums
+//  * @param {number[][]} operations
+//  * @return {number[]}
+//  */
+// var arrayChange = function(nums, operations) {
+//     let map = new Map();
+//     for(let i = 0; i < nums.length; i++){
+//         map.set(nums[i], i);
+//     }
+    
+//     let i = 0;
+//     while(i < operations.length){
+//         nums[map.get(operations[i][0])]  = operations[i][1]
+//         map.set(operations[i][1], map.get(operations[i][0]))
+//         i++;
+//     }
+    
+//     return nums
+// };
