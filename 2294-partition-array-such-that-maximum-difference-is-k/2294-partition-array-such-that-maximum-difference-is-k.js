3a2acb8ebebdbd706f8ee9542c0e4a430054f9d3
이시형 <59095793+SH-Lee2@users.noreply.github.com>
2022-06-05 13:12:52 +0900

    Time: 390 ms (100.00%), Space: 54.2 MB (100.00%) - LeetHub

--- 2294-partition-array-such-that-maximum-difference-is-k/2294-partition-array-such-that-maximum-difference-is-k.js
+++ 2294-partition-array-such-that-maximum-difference-is-k/2294-partition-array-such-that-maximum-difference-is-k.js
@@ -5,24 +5,12 @@
  */
 var partitionArray = function(nums, k) {
     nums.sort((a,b)=>b-a)
-    let cal = nums[0]
-    let maxNum = minNum = nums[0]
-    
+    let maxNum =  nums[0]
     let count = 1 
     for(let i=1; i<nums.length; i++){
-        // if(Math.max(...tmp) - Math.min(...tmp) > k) {
-        //     count++
-        //     tmp = [nums[i]]
-        // }
-        if(maxNum < nums[i]){
-            maxNum = nums[i]
-        }
-        if(minNum > nums[i]){
-            minNum = nums[i]
-        }
-        if(maxNum - minNum > k){
+        if(maxNum - nums[i] > k){
             count++
-            maxNum = minNum = nums[i]
+            maxNum  = nums[i]
         }
     }
     return count
