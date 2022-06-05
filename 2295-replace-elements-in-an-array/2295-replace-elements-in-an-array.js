eb7111e8cbfc28a33f08ed216204b8e2650ad795
이시형 <59095793+SH-Lee2@users.noreply.github.com>
2022-06-05 13:14:12 +0900

    Time: 253 ms (100.00%), Space: 91.6 MB (100.00%) - LeetHub

--- 2295-replace-elements-in-an-array/2295-replace-elements-in-an-array.js
+++ 2295-replace-elements-in-an-array/2295-replace-elements-in-an-array.js
@@ -4,18 +4,17 @@
  * @return {number[]}
  */
 var arrayChange = function(nums, operations) {
-    const list = {}
-    for(let i=0; i<nums.length;i++){
-        list[nums[i]] = i
+    let map = new Map();
+    for(let i = 0; i < nums.length; i++){
+        map.set(nums[i], i);
     }
-   for(let i=0; i<operations.length; i++){
-       const idx = list[operations[i][0]] === undefined ?   -1 : list[operations[i][0]]
-       if(idx >= 0 ){
-           nums[idx] = operations[i][1]
-           delete list[operations[i][0]]
-           list[operations[i][1]] = idx
-       }
-   }
-   
+    
+    let i = 0;
+    while(i < operations.length){
+        nums[map.get(operations[i][0])]  = operations[i][1]
+        map.set(operations[i][1], map.get(operations[i][0]))
+        i++;
+    }
+    
     return nums
 };
