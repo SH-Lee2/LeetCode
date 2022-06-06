f81438beecbbea3d0ccfb13ab473b8a7d7c7f649
이시형 <59095793+SH-Lee2@users.noreply.github.com>
2022-06-06 16:03:34 +0900

    Time: 125 ms (48.06%), Space: 49.9 MB (55.66%) - LeetHub

---
+++ 160-intersection-of-two-linked-lists/160-intersection-of-two-linked-lists.js
@@ -0,0 +1,24 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+
+/**
+ * @param {ListNode} headA
+ * @param {ListNode} headB
+ * @return {ListNode}
+ */
+var getIntersectionNode = function(headA, headB) {
+    if(headA === null) return 
+    if(headB === null) return 
+    let curA = headA
+    let curB = headB
+    while(curA !== curB){
+        curA = curA === null ? headB : curA.next
+        curB = curB === null ? headA : curB.next
+    }
+    return curA
+};
