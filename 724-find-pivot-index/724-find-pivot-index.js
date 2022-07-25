e04442dce58b4d3217d8acd225c69558b22bc437
이시형 <tlgud585@gmail.com>
2022-07-25 23:57:21 +0900

    Time: 101 ms (73.90%), Space: 44.5 MB (56.95%) - LeetHub

--- 724-find-pivot-index/724-find-pivot-index.js
+++ 724-find-pivot-index/724-find-pivot-index.js
@@ -5,11 +5,16 @@
 var pivotIndex = function(nums) {
   let leftTotal = 0 
   let rightTotal = nums.reduce((a,b)=>a+b,0) - nums[0] 
-  if(leftTotal === rightTotal) return 0
+  if(check(leftTotal,rightTotal)) return 0
   for(let i=1; i<nums.length; i++){
         leftTotal += nums[i-1]
         rightTotal -= nums[i]
-        if(leftTotal === rightTotal) return i
+        if(check(leftTotal,rightTotal)) return i
   }
     return -1 
 };
+
+function check(left, right){
+    if(left === right) return true 
+    return false
+}
