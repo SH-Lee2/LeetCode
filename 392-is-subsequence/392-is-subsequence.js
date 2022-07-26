6b3bee679d148e6183b58ff05cbec2517db45b36
이시형 <tlgud585@gmail.com>
2022-07-26 14:36:15 +0900

    Time: 76 ms (75.08%), Space: 42.4 MB (30.95%) - LeetHub

---
+++ 392-is-subsequence/392-is-subsequence.js
@@ -0,0 +1,12 @@
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var isSubsequence = function(s, t) {
+    let sIdx = 0 
+    for(let i=0; i<t.length; i++){
+        if(s[sIdx] === t[i]) sIdx++
+    }
+    return sIdx === s.length ? true : false
+};
