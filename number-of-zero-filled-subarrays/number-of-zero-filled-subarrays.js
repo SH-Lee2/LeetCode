3a3108c698b62fe5933f31d81376923ce9b00fde
이시형 <tlgud585@gmail.com>
2022-09-26 05:10:50 +0900

    Time: 138 ms, Memory: 52.9 MB - LeetHub

--- number-of-zero-filled-subarrays/number-of-zero-filled-subarrays.js
+++ number-of-zero-filled-subarrays/number-of-zero-filled-subarrays.js
@@ -4,7 +4,7 @@
  */
 var zeroFilledSubarray = function(nums) {
     let res = 0;
-    for (let i = 0, j = 0; i < nums.length; ++i) {
+    for (let i = 0, j = 0; i < nums.length; i++) {
         if (nums[i] != 0)
             j = i + 1;
          res += i - j + 1;
