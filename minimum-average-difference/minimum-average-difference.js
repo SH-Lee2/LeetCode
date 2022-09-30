2021b6a77641b7795f2ea0cb4f9dc527f0833cd1
이시형 <tlgud585@gmail.com>
2022-10-01 03:25:30 +0900

    Time: 108 ms, Memory: 52.1 MB - LeetHub

--- minimum-average-difference/minimum-average-difference.js
+++ minimum-average-difference/minimum-average-difference.js
@@ -33,37 +33,4 @@ var minimumAverageDifference = function(nums) {
     }
     
     return minIdx
-    
-    
-//     const MAX = Number.MAX_SAFE_INTEGER;
-//     const n = nums.length;
-    
-//     let totSum = nums.reduce((acc, num) => acc + num, 0);
-    
-//     let leftSum = 0;
-    
-//     let minIdx = -1;
-//     let minDiff = MAX;
-    
-//     for (let i = 0; i < n; ++i) {
-//         const currNum = nums[i];
-        
-//         leftSum += currNum;
-//         totSum -= currNum;
-        
-//         const leftSize = i + 1;
-//         const rightSize = n - i - 1;
-        
-//         const leftAvg = Math.floor(leftSum / leftSize);
-//         const rightAvg = rightSize === 0 ? 0 : Math.floor(totSum / rightSize);
-        
-//         const absDiff = Math.abs(leftAvg - rightAvg);
-        
-//         if (absDiff < minDiff) {
-//             minIdx = i;
-//             minDiff = absDiff;
-//         }
-//     }
-    
-//     return minIdx;
 };
