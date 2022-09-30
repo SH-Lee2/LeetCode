d38e2461ca25d77661936330f4194da1b95a041d
이시형 <tlgud585@gmail.com>
2022-10-01 05:31:29 +0900

    Time: 121 ms (42.04%), Space: 45.1 MB (50.49%) - LeetHub

--- 141-linked-list-cycle/141-linked-list-cycle.js
+++ 141-linked-list-cycle/141-linked-list-cycle.js
@@ -10,6 +10,10 @@
  * @param {ListNode} head
  * @return {boolean}
  */
+
+// 경마장의 빠른 말과 느린 말을 생각해보면 쉽다. 
+// 빠른 말이 느린말을 따라잡는다면 그 경마장은 원형 사이클이란 뜻이다.
+// 하지만 못 따라잡는다면 원형 사이클이 아니다.
 var hasCycle = function(head) {
     let slow = head
     let fast = head
