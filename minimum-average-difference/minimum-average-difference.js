885208de3f4eb86a9ea421750da20365de7b8187
이시형 <tlgud585@gmail.com>
2022-10-01 03:25:04 +0900

    Time: 139 ms, Memory: 53.6 MB - LeetHub

--- minimum-average-difference/minimum-average-difference.js
+++ minimum-average-difference/minimum-average-difference.js
@@ -3,53 +3,67 @@
  * @return {number}
  */
 var minimumAverageDifference = function(nums) {
-//     let left = nums[0]
-//     let leftIdx = 1
-//     let right = 0
-//     let rightIdx = nums.length - 1
-//     for(let i=1; i<nums.length; i++) right += nums[i]
-//     let min = Math.abs(Math.floor(left/ leftIdx) - Math.floor(right/rightIdx)) 
-//     let minIdx = 0
-//     for(let i=1; i<nums.length; i++){
-//         left = (left + nums[i] )
-//         right = (right - nums[i])
-//         const abs = Math.abs(Math.floor(left / ++leftIdx) - Math.floor(right / --rightIdx))
-//         if(min >abs) {
-//             min = abs
-//             minIdx = i
-//         }    
-//     }
-    
-//     return minIdx
     const MAX = Number.MAX_SAFE_INTEGER;
-    const n = nums.length;
+    let len = nums.length
     
-    let totSum = nums.reduce((acc, num) => acc + num, 0);
+    let totalSum = nums.reduce((acc, num)=> acc + num, 0)
+    let leftSum = 0 
     
-    let leftSum = 0;
+    let min = MAX
+    let minIdx = -1
     
-    let minIdx = -1;
-    let minDiff = MAX;
-    
-    for (let i = 0; i < n; ++i) {
-        const currNum = nums[i];
+    for(let i=0; i<len; i++){
+        const currentNum = nums[i] 
         
-        leftSum += currNum;
-        totSum -= currNum;
+        leftSum += currentNum
+        totalSum -= currentNum
         
-        const leftSize = i + 1;
-        const rightSize = n - i - 1;
+        const leftSize = i + 1
+        const rightSize = len - i - 1
         
-        const leftAvg = Math.floor(leftSum / leftSize);
-        const rightAvg = rightSize === 0 ? 0 : Math.floor(totSum / rightSize);
+        const leftAvg = Math.floor(leftSum / leftSize)
+        const rightAvg = rightSize === 0 ? 0 : Math.floor(totalSum / rightSize)
         
-        const absDiff = Math.abs(leftAvg - rightAvg);
+        const abs = Math.abs(leftAvg - rightAvg) 
         
-        if (absDiff < minDiff) {
-            minIdx = i;
-            minDiff = absDiff;
+        if(min > abs) {
+            min = abs 
+            minIdx = i
         }
     }
     
-    return minIdx;
+    return minIdx
+    
+    
+//     const MAX = Number.MAX_SAFE_INTEGER;
+//     const n = nums.length;
+    
+//     let totSum = nums.reduce((acc, num) => acc + num, 0);
+    
+//     let leftSum = 0;
+    
+//     let minIdx = -1;
+//     let minDiff = MAX;
+    
+//     for (let i = 0; i < n; ++i) {
+//         const currNum = nums[i];
+        
+//         leftSum += currNum;
+//         totSum -= currNum;
+        
+//         const leftSize = i + 1;
+//         const rightSize = n - i - 1;
+        
+//         const leftAvg = Math.floor(leftSum / leftSize);
+//         const rightAvg = rightSize === 0 ? 0 : Math.floor(totSum / rightSize);
+        
+//         const absDiff = Math.abs(leftAvg - rightAvg);
+        
+//         if (absDiff < minDiff) {
+//             minIdx = i;
+//             minDiff = absDiff;
+//         }
+//     }
+    
+//     return minIdx;
 };
