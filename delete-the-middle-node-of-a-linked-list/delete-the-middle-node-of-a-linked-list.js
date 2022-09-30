73b506bc437843da0fcd5dba8db23a49e0958df1
이시형 <tlgud585@gmail.com>
2022-10-01 06:20:49 +0900

    Time: 1519 ms, Memory: 127.4 MB - LeetHub

---
+++ delete-the-middle-node-of-a-linked-list/delete-the-middle-node-of-a-linked-list.js
@@ -0,0 +1,35 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+
+/**
+  * 중앙에 위치하는 노드를 찾고 삭제한다.
+*/
+var deleteMiddle = function(head) {
+    if(!head.next) return head.next
+    
+    let slow = head
+    let fast = head
+    let preNode = head
+    
+    while(fast && fast.next){
+        preNode = slow
+        slow = slow.next
+        fast = fast.next.next
+    }
+    
+    const nodeToRemove = slow 
+    preNode.next = nodeToRemove.next 
+    nodeToRemove.next = null 
+    
+    return head
+    
+};
