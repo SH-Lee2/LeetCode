1c67e720c4f0bc9f1486724a6cb4281d520c5d2e
이시형 <tlgud585@gmail.com>
2022-10-03 05:51:04 +0900

    Time: 87 ms, Memory: 48.9 MB - LeetHub

---
+++ missing-number/missing-number.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var missingNumber = function(nums) {
+    const set = new Set()
+    
+    for(const num of nums){
+        set.add(num)
+    }
+    
+    for(let i=0; i<=nums.length; i++){
+        if(!set.has(i)) return i
+    }
+};
