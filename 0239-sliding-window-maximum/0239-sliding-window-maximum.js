5c72d1c8074104bb956b610edf5951d2f62a1112
이시형 <tlgud585@gmail.com>
2022-10-25 15:16:10 +0900

    Time: 1285 ms (20.06%), Space: 91.4 MB (38.54%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.js
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.js
@@ -3,6 +3,8 @@
  * @param {number} k
  * @return {number[]}
  */
+
+// monotonic
 var maxSlidingWindow = function(nums, k) {
     const queue = [] 
     const answer = []
@@ -14,10 +16,16 @@ var maxSlidingWindow = function(nums, k) {
         
         queue.push(i)
         
-       if (queue[0] + k == i) {
+        /**
+        * 만약 nums 배열이 [1,3,-1,-3,5,4,3,2] 라면
+        * queue에 [5,4,3,2] 가 남는다 이렇게 되면 k(3)을 넘게 되므로 5를 빼준다.
+        * 즉 위의 while문 조건에 걸리지 않고 k 이상일때 넘지 않도록 제거해주는 로직이다.
+        */
+       if (queue[0] + k === i) {
              queue.shift();
         }
         
+        // 처음 k개 이후에는 매번 answer에 push
         if(i >= k - 1) {
             answer.push(nums[queue[0]])
         }
