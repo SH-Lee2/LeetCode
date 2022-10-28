7458edd3b9faff0e4cb8ba7523420ffb10017ea6
이시형 <tlgud585@gmail.com>
2022-10-28 15:29:05 +0900

    Time: 135 ms (22.62%), Space: 44.3 MB (96.90%) - LeetHub

--- 0046-permutations/0046-permutations.js
+++ 0046-permutations/0046-permutations.js
@@ -3,27 +3,22 @@
  * @return {number[][]}
  */
 var permute = function(nums) {
-    const checkNums = Array.from({length:nums.length},()=>false) 
-    const ans = [] 
-    const tmp=[]
-    
-    const helper = (len) => {
-        if(len === nums.length) {
-            ans.push(tmp.slice())
-        }else{
-            for(let i=0; i<nums.length; i++){
-                if(!checkNums[i]){
-                    tmp[len] = nums[i]
-                    checkNums[i] = true 
-                    helper(len+1)
-                    checkNums[i] = false
-                }
-                
+    let backtrack = curr => {
+        if (curr.length == nums.length) {
+            ans.push([...curr]);
+            return;
+        }
+        
+        for (const num of nums) {
+            if (!curr.includes(num)) {
+                curr.push(num);
+                backtrack(curr);
+                curr.pop();
             }
         }
     }
-    helper(0)
-    return ans
+    
+    let ans = [];
+    backtrack([]);
+    return ans;
 };
-
-permute([1,2,3])
