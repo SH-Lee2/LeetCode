12dcec5485678a72a00e56850311f9770bd585fb
이시형 <tlgud585@gmail.com>
2022-10-30 21:13:01 +0900

    Time: 74 ms, Memory: 42.2 MB - LeetHub

---
+++ minimum-value-to-get-positive-step-by-step-sum/minimum-value-to-get-positive-step-by-step-sum.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var minStartValue = function(nums) {
+     // We use "total" for current step-by-step total, "minVal" for minimum 
+    // step-by-step total among all sums. Since we always start with 
+    // startValue = 0, therefore the initial current step-by-step total is 0, 
+    // thus we set "total" and "minVal" be 0. 
+    var minVal = 0;
+    var total = 0;
+
+    // Iterate over the array and get the minimum step-by-step total.
+    for (var i = 0; i < nums.length; ++i) {
+        total += nums[i];
+        minVal = Math.min(minVal, total);
+    }
+
+    // We have to let the minimum step-by-step total equals to 1, 
+    // by increasing the startValue from 0 to -minVal + 1, 
+    // which is just the minimum startValue we want.
+    return -minVal + 1;
+};
