4a2ed75d311f5124f54e1ca08ccc02cb1e440db6
이시형 <tlgud585@gmail.com>
2022-10-31 11:05:43 +0900

    Time: 112 ms (53.72%), Space: 43.9 MB (44.16%) - LeetHub

---
+++ 1189-maximum-number-of-balloons/1189-maximum-number-of-balloons.js
@@ -0,0 +1,22 @@
+/**
+ * @param {string} text
+ * @return {number}
+ */
+var maxNumberOfBalloons = function(text) {
+    const map = {
+        'b' : 0,
+        'a' : 0,
+        'l' : 0,
+        'o' : 0,
+        'n' : 0
+    }        
+    
+    for(const char of text) {
+        if(char in map) map[char] = (map[char] || 0) + 1
+    }
+    
+    map['l'] = Math.floor( map['l'] / 2)
+    map['o'] = Math.floor( map['o'] / 2) 
+    
+    return Math.min(...Object.values(map))
+};
