d5a7b87ac9fd3d70bfc184170d1cd15d6b9e1b9c
이시형 <tlgud585@gmail.com>
2022-10-31 14:41:57 +0900

    Time: 168 ms (93.83%), Space: 76.2 MB (59.26%) - LeetHub

--- 2260-minimum-consecutive-cards-to-pick-up/2260-minimum-consecutive-cards-to-pick-up.js
+++ 2260-minimum-consecutive-cards-to-pick-up/2260-minimum-consecutive-cards-to-pick-up.js
@@ -3,6 +3,8 @@
  * @return {number}
  */
 var minimumCardPickup = function(cards) {
+    // key의 인덱스 번호를 value로 저장하고 
+    // for문을 순회하면서 key값이 있다면 value 와 현재 인덱스 번호 차이를 비교한다.
     const map = new Map() 
     
     let min = Infinity
@@ -13,7 +15,7 @@ var minimumCardPickup = function(cards) {
         if(map.has(card)){
             min = Math.min(min, i - map.get(card))        
         }
-        map.set(card, i)
+        map.set(card, i) // 중복된 key값 대응
     }
     
     return min === Infinity ? -1 : min + 1
