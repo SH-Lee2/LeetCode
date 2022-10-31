688d5eb7fbae464011d4a4f0ebafd4a21e074fb1
이시형 <tlgud585@gmail.com>
2022-10-31 09:45:54 +0900

    Time: 617 ms (75.30%), Space: 121.2 MB (31.18%) - LeetHub

--- 2225-find-players-with-zero-or-one-losses/2225-find-players-with-zero-or-one-losses.js
+++ 2225-find-players-with-zero-or-one-losses/2225-find-players-with-zero-or-one-losses.js
@@ -4,20 +4,17 @@
  */
 var findWinners = function(matches) {
     const total = new Map()
-    const loser = new Map()
     
     for(const [win, lose] of matches){
-        total.set(win, (total.get(win)||0) + 1)
+        total.set(win, (total.get(win)||0))
         total.set(lose, (total.get(lose)||0) + 1)
-
-        loser.set(lose, (loser.get(lose)||0) + 1) 
     }
 
     const winnerArr = [] ,loserArr = []
     
     for(const [key, value] of total){
-        if(!loser.get(key)) winnerArr.push(key)
-        if(loser.get(key) === 1) loserArr.push(key)
+        if(total.get(key)===0) winnerArr.push(key)
+        if(total.get(key) === 1) loserArr.push(key)
     }
     
     winnerArr.sort((a,b)=> a-b)
