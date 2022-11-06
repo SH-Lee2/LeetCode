05329eebc2c423d4468feda01a61740dc1b1d383
이시형 <tlgud585@gmail.com>
2022-11-06 13:08:55 +0900

    Time: 111 ms (53.91%), Space: 45.6 MB (51.56%) - LeetHub

--- 1026-maximum-difference-between-node-and-ancestor/1026-maximum-difference-between-node-and-ancestor.js
+++ 1026-maximum-difference-between-node-and-ancestor/1026-maximum-difference-between-node-and-ancestor.js
@@ -11,19 +11,32 @@
  * @return {number}
  */
 var maxAncestorDiff = function(root) {
-    let ans = 0
+//     let ans = 0
+    
+//     const dfs = (node, max, min) => {
+//         if(!node) return 0 
+        
+//         max = Math.max(node.val, max)
+//         min = Math.min(node.val, min)
+        
+//         ans = Math.max(ans, Math.abs(max-min))
+//         dfs(node.left, max, min)
+//         dfs(node.right, max, min)
+        
+//     }
+//     dfs(root, 0, 5000)
+//     return ans
     
     const dfs = (node, max, min) => {
-        if(!node) return 0 
-        
+        if(!node) return max - min 
         max = Math.max(node.val, max)
         min = Math.min(node.val, min)
         
-        ans = Math.max(ans, Math.abs(max-min))
-        dfs(node.left, max, min)
-        dfs(node.right, max, min)
+        const left = dfs(node.left, max, min)
+        const right = dfs(node.right, max, min)
         
+        return Math.max(left, right)
     }
-    dfs(root, 0, 5000)
-    return ans
+    
+    return dfs(root, 0, 5000)
 };
