88684e55a5262f2c51337578fda99f932f21fb69
이시형 <tlgud585@gmail.com>
2022-11-13 10:26:06 +0900

    Time: 146 ms (72.05%), Space: 54.5 MB (74.11%) - LeetHub

---
+++ 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+var findMaxAverage = function(nums, k) {
+    let max = Number.MIN_SAFE_INTEGER
+    let sum = 0
+    
+    for(let i=0; i<k; i++){
+        sum += nums[i]
+    }
+    
+    max = Math.max(max, sum)
+    
+    for(let i=k; i<nums.length; i++){
+        sum = sum - nums[i-k] + nums[i]
+        max = Math.max(max, sum)
+    }
+    
+    return max/k
+};
