6004d65bcccfb842b137e60d7ff362c5692bca26
이시형 <tlgud585@gmail.com>
2022-11-15 13:21:15 +0900

    Time: 1974 ms (6.65%), Space: 184.4 MB (5.25%) - LeetHub

---
+++ 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.js
@@ -0,0 +1,35 @@
+/**
+ * @param {number} n
+ * @param {number[][]} edges
+ * @param {number} source
+ * @param {number} destination
+ * @return {boolean}
+ */
+var validPath = function(n, edges, source, destination) {
+    const graph = new Map()
+    
+    const dfs = (edge) => {
+        const node = graph.get(edge)
+        for(let i=0; i<node.length; i++){
+            const vertex = node[i]
+            if(!seen.has(vertex)){
+                seen.add(vertex)
+                dfs(vertex)
+            }
+        }
+    }
+    
+    for(let i=0; i<n; i++){
+        graph.set(i, [i])
+    }
+    
+    for(const [v1,v2] of edges){
+        graph.get(v1).push(v2)
+        graph.get(v2).push(v1)
+    }
+    
+    const seen = new Set()
+    
+    dfs(source)
+    return seen.has(destination)
+};
