31e1549e6b3c769cdf1d280ec91a675c498b3ffe
이시형 <tlgud585@gmail.com>
2022-11-16 15:07:58 +0900

    Time: 708 ms (64.65%), Space: 145.3 MB (49.50%) - LeetHub

--- 2368-reachable-nodes-with-restrictions/2368-reachable-nodes-with-restrictions.js
+++ 2368-reachable-nodes-with-restrictions/2368-reachable-nodes-with-restrictions.js
@@ -7,7 +7,7 @@
 var reachableNodes = function(n, edges, restricted) {
     const graph = new Map()
     const restrict = new Set(restricted)
-    const seen = new Set([0])
+    const seen = new Set()
     
     
     for(let i=0; i<n; i++){
@@ -19,18 +19,28 @@ var reachableNodes = function(n, edges, restricted) {
         graph.get(v2).push(v1)
     }
 
-    let ans = 1
+    let ans = 0
     
     const dfs = (node) => {
-        const edges = graph.get(node)
-        for(let i=0; i<edges.length; i++){
-            const vertex = edges[i]
-            if(restrict.has(vertex)) continue  // 접근 불가 
-            if(!seen.has(vertex)){
-                ans++
-                seen.add(vertex)
-                dfs(vertex)
-            }
+        // const edges = graph.get(node)
+        // for(let i=0; i<edges.length; i++){
+        //     const vertex = edges[i]
+        //     if(restrict.has(vertex)) continue  // 접근 불가 
+        //     if(!seen.has(vertex)){
+        //         ans++
+        //         seen.add(vertex)
+        //         dfs(vertex)
+        //     }
+        // }
+        if (restrict.has(node) || seen.has(node)) {
+            return;
+        }
+        
+        ans++;
+        seen.add(node);
+        
+        for (const vertex of graph.get(node)) {
+            dfs(vertex);
         }
     }
     
