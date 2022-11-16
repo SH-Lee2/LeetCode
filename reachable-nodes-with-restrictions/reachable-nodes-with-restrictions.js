fdbabc9ebc4b8dd664c2e759348dfa83db345769
이시형 <tlgud585@gmail.com>
2022-11-16 15:10:24 +0900

    Time: 784 ms, Memory: 144.7 MB - LeetHub

---
+++ reachable-nodes-with-restrictions/reachable-nodes-with-restrictions.js
@@ -0,0 +1,37 @@
+/**
+ * @param {number} n
+ * @param {number[][]} edges
+ * @param {number[]} restricted
+ * @return {number}
+ */
+var reachableNodes = function(n, edges, restricted) {
+    const graph = new Map()
+    const restrict = new Set(restricted)
+    const seen = new Set([0])
+    
+    
+    for(let i=0; i<n; i++){
+        graph.set(i,[])
+    }
+    
+    for(const [v1,v2] of edges){
+        graph.get(v1).push(v2)
+        graph.get(v2).push(v1)
+    }
+
+    let ans = 1
+    
+    const dfs = (node) => {
+        for(const vertex of graph.get(node)){
+            if(restrict.has(vertex)) continue  // 접근 불가 
+            if(!seen.has(vertex)){
+                ans++
+                seen.add(vertex)
+                dfs(vertex)
+            }
+        }
+    }
+    
+    dfs(0)
+    return ans
+};
