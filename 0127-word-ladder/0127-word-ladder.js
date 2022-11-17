8015c13b65856d8069593871aea5f6527080840c
이시형 <tlgud585@gmail.com>
2022-11-18 03:09:13 +0900

    Time: 501 ms (36.11%), Space: 50.4 MB (55.43%) - LeetHub

--- 0127-word-ladder/0127-word-ladder.js
+++ 0127-word-ladder/0127-word-ladder.js
@@ -9,14 +9,14 @@ var ladderLength = function(beginWord, endWord, wordList) {
     
     if(!wordSet.has(endWord)) return 0
     
-    const s = new Set()
-    for(const word of wordList) {
-        for(let i=0; i<word.length; i++){
-            s.add(word[i])
-        }
-    }
+//     const s = new Set()
+//     for(const word of wordList) {
+//         for(let i=0; i<word.length; i++){
+//             s.add(word[i])
+//         }
+//     }
     
-    const choices = [...s]
+//     const choices = [...s]
     let queue = [beginWord]
     let step = 0
     const seen = new Set(beginWord)
@@ -26,10 +26,10 @@ var ladderLength = function(beginWord, endWord, wordList) {
         for(const word of queue){
             if(word === endWord) return step+1
             
-            for(const choice of choices){
-                for(let i=0; i<word.length; i++){
+            for(let i=0; i<26; i++){
+                for(let j=0; j<word.length; j++){
                     
-                    const newWord = word.substring(0,i) + choice + word.substring(i+1)
+                    const newWord = word.substring(0,j) + String.fromCharCode(i + 97) + word.substring(j+1)
                     if(!seen.has(newWord) && wordSet.has(newWord)){
                         seen.add(newWord)
                         nextQueue.push(newWord)
