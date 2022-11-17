f8ec99a24ff6742f4faab3f1e8b28915d3d74f1a
이시형 <tlgud585@gmail.com>
2022-11-18 03:09:51 +0900

    Time: 518 ms (34.23%), Space: 50.6 MB (48.12%) - LeetHub

--- 0127-word-ladder/0127-word-ladder.js
+++ 0127-word-ladder/0127-word-ladder.js
@@ -8,15 +8,7 @@ var ladderLength = function(beginWord, endWord, wordList) {
     const wordSet = new Set(wordList)
     
     if(!wordSet.has(endWord)) return 0
-    
-//     const s = new Set()
-//     for(const word of wordList) {
-//         for(let i=0; i<word.length; i++){
-//             s.add(word[i])
-//         }
-//     }
-    
-//     const choices = [...s]
+
     let queue = [beginWord]
     let step = 0
     const seen = new Set(beginWord)
