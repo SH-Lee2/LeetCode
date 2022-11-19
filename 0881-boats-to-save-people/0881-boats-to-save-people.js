6a9b98b345e13371ab9a1aba35c7796a7554f1ce
이시형 <tlgud585@gmail.com>
2022-11-19 21:05:03 +0900

    Time: 324 ms (24.03%), Space: 50.5 MB (15.88%) - LeetHub

--- 0881-boats-to-save-people/0881-boats-to-save-people.js
+++ 0881-boats-to-save-people/0881-boats-to-save-people.js
@@ -4,28 +4,24 @@
  * @return {number}
  */
 var numRescueBoats = function(people, limit) {
-    /**
-    * 최대 2명! 한도는 2명의 합 <= limit
-    * 투 포인트 알고리즘으로 푼다.
-    */
+    people.sort((a,b)=>a-b) 
+    let left = 0 
+    let right = people.length -1 
     
-    //people 오름차순 정렬
-    people.sort((a,b) => a-b)
-    // boat 개수를 담는 변수 초기화
-    let boatCount = 0
-    // left, right 변수 초기화
-    let left = 0, right = people.length -1
-    // while(left <= right) 순회
-    while(left <= right) {
-        // people[left] + people[right] > limit 이면 boat+1, right-- 
-        if(people[left] + people[right] > limit) {
-            right--
-        }else{
-        // people[left] + people[right] <= limit 이면 boat + 1, left++, right-- 
+    let boat = 0
+    while(left <= right){
+        const sum = people[left] + people[right]
+        
+        if(sum <= limit){
             left++
             right--
+            boat++
         }
-        boatCount++
+        else{
+            right--
+            boat++
+        }
     }
-    return boatCount    
+    
+    return boat
 };
