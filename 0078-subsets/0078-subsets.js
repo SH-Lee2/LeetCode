52335fd5788f53c7570fe4ca915f1ee145c265b2
이시형 <tlgud585@gmail.com>
2022-11-23 23:24:22 +0900

    Time: 90 ms (74.96%), Space: 42.7 MB (91.37%) - LeetHub

--- 0078-subsets/0078-subsets.js
+++ 0078-subsets/0078-subsets.js
@@ -3,20 +3,20 @@
  * @return {number[][]}
  */
 var subsets = function(nums) {
-       let backtrack = (curr, i) => {
+    let backtrack = (curr, i) => {
         if (i > nums.length) {
             return;
         }
         
-        ans.push([...curr]);
-        for (let j = i; j < nums.length; j++) {
-            curr.push(nums[j]);
-            backtrack(curr, j + 1);
-            curr.pop();
+        ans.push([...curr])
+        for(let j=i; j<nums.length; j++){
+            curr.push(nums[j])
+            backtrack(curr,j+1)
+            curr.pop()
         }
     }
     
     let ans = [];
     backtrack([], 0);
-    return ans;
+    return ans
 };
