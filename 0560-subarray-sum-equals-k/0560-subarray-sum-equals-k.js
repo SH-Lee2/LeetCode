90ce0a674ec64b3af96082630063433cdb94e5fe
이시형 <tlgud585@gmail.com>
2022-11-25 23:38:50 +0900

    Time: 137 ms (64.87%), Space: 50.9 MB (50.32%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.js
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.js
@@ -4,21 +4,15 @@
  * @return {number}
  */
 var subarraySum = function(nums, k) {
+    let ans = 0 
     const map = new Map()
+    map.set(0,1)
     
-    // 빈배열 []의 하위배열 합은 0
-    map.set(0,1) 
-    let curr = 0 
-    let ans = 0
-    
-    /**
-    * prefix sum을 통해 더한값(curr)의 - k 값이 해시에 저장되어 있는경우
-    * nums 하위배열의 합이 k와 일치한다고 본다.
-    */
+    let curr = 0
     for(const num of nums){
-        curr += num
-        ans += (map.get(curr-k) || 0)
-        map.set(curr, (map.get(curr)|| 0) + 1)
+        curr+= num 
+        ans += (map.get(curr-k)||0)
+        map.set(curr,(map.get(curr) || 0) + 1)
     }
     
     return ans
