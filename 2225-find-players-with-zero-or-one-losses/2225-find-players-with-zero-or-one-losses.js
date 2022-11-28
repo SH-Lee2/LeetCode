4401e1dafeba8352c5ca33915a9283d93cf1f638
이시형 <tlgud585@gmail.com>
2022-11-28 13:30:54 +0900

    Time: 567 ms (79.72%), Space: 122 MB (26.57%) - LeetHub

--- 2225-find-players-with-zero-or-one-losses/2225-find-players-with-zero-or-one-losses.js
+++ 2225-find-players-with-zero-or-one-losses/2225-find-players-with-zero-or-one-losses.js
@@ -3,30 +3,24 @@
  * @return {number[][]}
  */
 var findWinners = function(matches) {
-    const winner = new Map()
-    const loser = new Map()
+    const total = new Map()
     
     for(const match of matches){
         const [win, lose] = match
         
-        winner.set(win,(winner.get(win)||0)+1)
-        loser.set(lose,(loser.get(lose)||0)+1)
-        
-        
-        // if(winner.has(win) && loser.has(win)) winner.delete(win)
+        total.set(win, (total.get(win)||0))
+        total.set(lose, (total.get(lose)||0)+1)
     }
     
-    let left = [] 
-    for(const key of winner.keys()){
-        if(!loser.has(key)) left.push(key)
-    }
-    let right = [] 
-    for(const [key, value] of loser){
-        if(value === 1) right.push(key)
+    const winner = [] , loser = []
+    
+    for(const [key, value] of total){
+        if(value === 0) winner.push(key)
+        if(value === 1) loser.push(key)
     }
     
-    left.sort((a,b)=>a-b)
-    right.sort((a,b)=>a-b)
+    winner.sort((a,b)=>a-b)
+    loser.sort((a,b)=>a-b)
     
-    return [left, right]
+    return [winner, loser]
 };
