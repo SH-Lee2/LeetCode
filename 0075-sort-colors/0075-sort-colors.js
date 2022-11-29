9d68e1910384d8f58032ca3b24f4889b6cf13c24
이시형 <tlgud585@gmail.com>
2022-11-29 15:31:30 +0900

    Time: 58 ms (98.15%), Space: 41.9 MB (70.64%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.js
+++ 0075-sort-colors/0075-sort-colors.js
@@ -3,5 +3,11 @@
  * @return {void} Do not return anything, modify nums in-place instead.
  */
 var sortColors = function(nums) {
-    nums.sort((a,b)=>a-b)
+    for(let i=0; i<nums.length; i++){
+        for(let j=i+1; j<nums.length; j++){
+            if(nums[i] > nums[j]){
+                [nums[i],nums[j]] = [nums[j],nums[i]]
+            }
+        }
+    }
 };
