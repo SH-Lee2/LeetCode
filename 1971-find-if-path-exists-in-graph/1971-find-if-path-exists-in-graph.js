9da200dcf7e407cc81f5d37f16ea737815be9894
이시형 <tlgud585@gmail.com>
2022-12-19 14:23:57 +0900

    Time: 873 ms (44.43%), Space: 179.9 MB (20.03%) - LeetHub

--- 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.js
+++ 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.js
@@ -8,30 +8,30 @@
 var validPath = function(n, edges, source, destination) {
     const graph = new Map()
     
-    const dfs = (edge) => {
-        if(edge === destination) return true
-        
-        const node = graph.get(edge)
-        for(let i=0; i<node.length; i++){
-            const vertex = node[i]
-            if(!seen.has(vertex)){
-                seen.add(vertex)
-                if(dfs(vertex)) return true
-            }
-        }
-        return false 
-    }
-    
-    for(let i=0; i<n; i++){
-        graph.set(i, [i])
-    }
-    
-    for(const [v1,v2] of edges){
+    edges.forEach(edge=>{
+        const [v1, v2] = edge
+        if(!graph.has(v1)) graph.set(v1,[])
+        if(!graph.has(v2)) graph.set(v2,[])
         graph.get(v1).push(v2)
         graph.get(v2).push(v1)
-    }
+    })
     
+    let queue = [source]
     const seen = new Set()
     
-    return dfs(source)
+    while(queue.length){
+        const nextQueue = [] 
+        for(const vertex of queue){
+            if(vertex === destination) return true
+            seen.add(vertex)
+            for(const edge of graph.get(vertex)){
+                if(!seen.has(edge)){
+                    nextQueue.push(edge)
+                }                
+            }
+        }
+        queue = nextQueue
+    }
+    
+    return false
 };
