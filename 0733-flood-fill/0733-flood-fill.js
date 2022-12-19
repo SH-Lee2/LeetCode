1c939711b17f1f8430673f41982c6c7b521eafc5
이시형 <tlgud585@gmail.com>
2022-12-20 04:09:50 +0900

    Time: 108 ms (68.33%), Space: 44.7 MB (27.39%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.js
+++ 0733-flood-fill/0733-flood-fill.js
@@ -6,34 +6,53 @@
  * @return {number[][]}
  */
 var floodFill = function(image, sr, sc, color) {
-    const direction = [[-1,0],[0,1],[1,0],[0,-1]]
+    
+// BFS
+//     const direction = [[-1,0],[0,1],[1,0],[0,-1]]
+//     const n = image.length
+//     const m = image[0].length
+//     const defaultColor = image[sr][sc]
+//     if(defaultColor === color) return image
+    
+//     const isValid = (x,y) => {
+//         return x >= 0 && x < n && y >= 0 && y < m && image[x][y] === defaultColor
+//     }
+    
+//     let queue = [[sr,sc]]
+//     image[sr][sc] = color
+//     while(queue.length){
+//         const nextQueue = [] 
+//         for(const [x,y] of queue){
+//             for(const [dx,dy] of direction){
+//                 const nx = dx + x
+//                 const ny = dy + y
+                
+//                 if(isValid(nx,ny)){
+//                     image[nx][ny] = color 
+//                     nextQueue.push([nx,ny])
+//                 }
+//             }
+//         }
+        
+//         queue = nextQueue
+//     }
+    
+//     return image
+    
+// DFS 
     const n = image.length
     const m = image[0].length
     const defaultColor = image[sr][sc]
     if(defaultColor === color) return image
-    
-    const isValid = (x,y) => {
-        return x >= 0 && x < n && y >= 0 && y < m && image[x][y] === defaultColor
-    }
-    
-    let queue = [[sr,sc]]
-    image[sr][sc] = color
-    while(queue.length){
-        const nextQueue = [] 
-        for(const [x,y] of queue){
-            for(const [dx,dy] of direction){
-                const nx = dx + x
-                const ny = dy + y
-                
-                if(isValid(nx,ny)){
-                    image[nx][ny] = color 
-                    nextQueue.push([nx,ny])
-                }
-            }
-        }
+    const dfs = (x,y) => {
+        if(x < 0 || x >= n || y < 0 || y >= m || image[x][y] !== defaultColor) return
         
-        queue = nextQueue
+        image[x][y] = color
+        
+        return dfs(x-1, y) || dfs(x, y+1) || dfs(x+1, y) || dfs(x, y-1)
     }
+    
+    dfs(sr,sc)
     
     return image
 };
