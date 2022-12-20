837caed8f3d7bd52e528473cbfe332262a46b010
이시형 <tlgud585@gmail.com>
2022-12-20 13:25:22 +0900

    Time: 82 ms (73.81%), Space: 44.7 MB (27.68%) - LeetHub

--- 0841-keys-and-rooms/0841-keys-and-rooms.js
+++ 0841-keys-and-rooms/0841-keys-and-rooms.js
@@ -3,19 +3,16 @@
  * @return {boolean}
  */
 var canVisitAllRooms = function(rooms) {
-    const unlocks = new Array(rooms.length).fill(false)
     if(rooms[0].length === 0) return false 
     
     let queue = [0]
     const seen = new Set([0])
-    unlocks[0] = true
     
     while(queue.length){
         const nextQueue = [] 
         for(const room of queue){
             for(const key of rooms[room]){
                 if(!seen.has(key)){
-                    unlocks[key] = true 
                     seen.add(key)
                     nextQueue.push(key)
                 }
@@ -25,6 +22,5 @@ var canVisitAllRooms = function(rooms) {
         queue = nextQueue 
     }
     
-    console.log(unlocks)
-    return unlocks.every(lock => lock === true)
+    return seen.size === rooms.length
 };
