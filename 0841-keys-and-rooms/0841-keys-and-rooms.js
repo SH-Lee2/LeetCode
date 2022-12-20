78bb125d9e7b5bed9689d7742f007fd27a239df5
이시형 <tlgud585@gmail.com>
2022-12-20 13:27:59 +0900

    Time: 99 ms (55.36%), Space: 44.9 MB (18.75%) - LeetHub

--- 0841-keys-and-rooms/0841-keys-and-rooms.js
+++ 0841-keys-and-rooms/0841-keys-and-rooms.js
@@ -3,24 +3,41 @@
  * @return {boolean}
  */
 var canVisitAllRooms = function(rooms) {
+// BFS 
+//     if(rooms[0].length === 0) return false 
+    
+//     let queue = [0]
+//     const seen = new Set([0])
+    
+//     while(queue.length){
+//         const nextQueue = [] 
+//         for(const room of queue){
+//             for(const key of rooms[room]){
+//                 if(!seen.has(key)){
+//                     seen.add(key)
+//                     nextQueue.push(key)
+//                 }
+//             }
+//         }
+        
+//         queue = nextQueue 
+//     }
+    
+//     return seen.size === rooms.length]
+    
+// DFS
+    const seen = new Set([0])
     if(rooms[0].length === 0) return false 
     
-    let queue = [0]
-    const seen = new Set([0])
-    
-    while(queue.length){
-        const nextQueue = [] 
-        for(const room of queue){
-            for(const key of rooms[room]){
-                if(!seen.has(key)){
-                    seen.add(key)
-                    nextQueue.push(key)
-                }
+    const dfs = (room) => {
+        for(const key of rooms[room]){
+            if(!seen.has(key)){
+                seen.add(key)
+                dfs(key)
             }
         }
-        
-        queue = nextQueue 
     }
+    dfs(0)
     
     return seen.size === rooms.length
 };
