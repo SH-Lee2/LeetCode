de4791824e267f44fe6086a7f2a43233933f2967
이시형 <tlgud585@gmail.com>
2022-12-31 23:33:44 +0900

    Time: 460 ms (93.86%), Space: 83.7 MB (95.06%) - LeetHub

--- 2225-find-players-with-zero-or-one-losses/2225-find-players-with-zero-or-one-losses.js
+++ 2225-find-players-with-zero-or-one-losses/2225-find-players-with-zero-or-one-losses.js
@@ -3,22 +3,23 @@
  * @return {number[][]}
  */
 var findWinners = function(matches) {
-    const total = new Map()
+    // [한번도 패배하지 않은 사람들, 딱 한번만 패매한 사람들]
+     const total = new Map()
     
-    for(const match of matches){
-        const [win, lose] = match
-        
-        total.set(win, (total.get(win)||0))
-        total.set(lose, (total.get(lose)||0)+1)
+    for(const [winner, loser] of matches){
+        total.set(winner, (total.get(winner)||0))
+        total.set(loser, (total.get(loser)||0)+1)
     }
     
     const winner = [] , loser = []
     
+    // answer[0] = 0, answer[1] = 1 그 외에는 조건에 부합하지 않는다. 
     for(const [key, value] of total){
         if(value === 0) winner.push(key)
         if(value === 1) loser.push(key)
     }
     
+    // 오름차순 정렬
     winner.sort((a,b)=>a-b)
     loser.sort((a,b)=>a-b)
     
