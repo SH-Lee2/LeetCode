f5f4b45b618ef78675be9b9b2ae59f957cf86f90
이시형 <tlgud585@gmail.com>
2023-01-06 14:39:49 +0900

    Time: 142 ms (37.98%), Space: 49.4 MB (6.33%) - LeetHub

--- 0930-binary-subarrays-with-sum/0930-binary-subarrays-with-sum.js
+++ 0930-binary-subarrays-with-sum/0930-binary-subarrays-with-sum.js
@@ -4,16 +4,15 @@
  * @return {number}
  */
 var numSubarraysWithSum = function(nums, goal) {
-     let ans = 0 
-     const len = nums.length
-     for(let i=0; i<len; i++){
-         let sum = 0
-         for(let j=i; j<len; j++){
-             sum+= nums[j]
-             if(sum === goal) ans++
-             if(sum > goal) break
-         }
-     }
-    
-    return ans
+    let counts = new Map();
+    counts.set(0, 1);
+    let ans = 0, curr = 0;
+
+    for (const num of nums) {
+        curr += num;
+        ans += counts.get(curr - goal) || 0;
+        counts.set(curr, (counts.get(curr) || 0) + 1);
+    }
+
+    return ans;
 };
