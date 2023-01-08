def99821e502d84d2ca1f61b480fe77cbe409d0f
이시형 <tlgud585@gmail.com>
2023-01-08 22:16:16 +0900

    Time: 105 ms (35.29%), Space: 48.5 MB (9.41%) - LeetHub

--- 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.js
+++ 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.js
@@ -7,9 +7,6 @@ var gcdOfStrings = function(str1, str2) {
     let prefix = ''
     let ans = ''
     
-//     const minStr = str1.length > str2.length ? str2 : str1
-//     const maxStr = str1.length > str2.length ? str1 : str2
-    
     for(const char of str2){
         prefix += char 
         if(str1.replaceAll(prefix,'') === '' && str2.replaceAll(prefix, '') === ''){
