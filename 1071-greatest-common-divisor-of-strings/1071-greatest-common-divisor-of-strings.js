aa2079465dfd0863ae98e0449edfbd0f1459240b
이시형 <tlgud585@gmail.com>
2023-01-08 22:14:35 +0900

    Time: 91 ms (64.71%), Space: 48.7 MB (7.06%) - LeetHub

---
+++ 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.js
@@ -0,0 +1,21 @@
+/**
+ * @param {string} str1
+ * @param {string} str2
+ * @return {string}
+ */
+var gcdOfStrings = function(str1, str2) {
+    let prefix = ''
+    let ans = ''
+    
+    const minStr = str1.length > str2.length ? str2 : str1
+    const maxStr = str1.length > str2.length ? str1 : str2
+    
+    for(const char of minStr){
+        prefix += char 
+        if(maxStr.replaceAll(prefix,'') === '' && minStr.replaceAll(prefix, '') === ''){
+            ans = prefix.length > ans.length ? prefix : ans 
+        } 
+    }
+    
+    return ans 
+};
