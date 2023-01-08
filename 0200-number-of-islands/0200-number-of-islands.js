7330fd8fd3119f8ea3a2b951aedfbd7b96640ce4
이시형 <tlgud585@gmail.com>
2023-01-08 21:51:05 +0900

    Time: 88 ms (84.27%), Space: 50.2 MB (32.31%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.js
+++ 0200-number-of-islands/0200-number-of-islands.js
@@ -3,9 +3,9 @@
  * @return {number}
  */
 var numIslands = function(grid) {
-    // const isValid = (x,y) => {
-    //     return x>=0 && y>=0 && x<grid.length && y< grid[0].length && grid[x][y]==="1"
-    // }
+    const isValid = (x,y) => {
+        return x>=0 && y>=0 && x<grid.length && y< grid[0].length && grid[x][y]==="1"
+    }
     // function DFS(x,y){
     //     if(isValid(x,y)){
     //         grid[x][y] = 0
@@ -24,10 +24,7 @@ var numIslands = function(grid) {
     // return count
     
     let count = 0 
-    
-    const isValid = (x,y) => {
-        return x < 0 || x >= grid.length || y < 0 || y >= grid[0].length || grid[x][y] === '0' 
-    }
+
     const directions = [[-1,0],[0,1],[1,0],[0,-1]]
     
     const BFS = (i,j) => {
@@ -37,7 +34,7 @@ var numIslands = function(grid) {
             const nextQueue = []
 
             for(const [x,y] of queue){
-                if(isValid(x,y)) continue
+                if(!isValid(x,y)) continue
                 grid[x][y] = '0'
                 for(const [dx, dy] of directions){
                     const nx = x + dx
