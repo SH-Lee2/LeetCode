7f1b6b1dfcec343a7fa05e7b009ac4cf91370763
이시형 <tlgud585@gmail.com>
2023-01-08 22:19:39 +0900

    Time: 77 ms (78.82%), Space: 47.4 MB (17.65%) - LeetHub

--- 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.js
+++ 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.js
@@ -4,15 +4,23 @@
  * @return {string}
  */
 var gcdOfStrings = function(str1, str2) {
-    let prefix = ''
-    let ans = ''
+//     let prefix = ''
+//     let ans = ''
     
-    for(const char of str2){
-        prefix += char 
-        if(str1.replaceAll(prefix,'') === '' && str2.replaceAll(prefix, '') === ''){
-            ans = prefix.length > ans.length ? prefix : ans 
-        } 
+//     for(const char of str2){
+//         prefix += char 
+//         if(str1.replaceAll(prefix,'') === '' && str2.replaceAll(prefix, '') === ''){
+//             ans = prefix.length > ans.length ? prefix : ans 
+//         } 
+//     }
+    
+//     return ans 
+    const [smallerString, largerString] = [str1, str2].sort((a,b) => a.length - b.length);
+    for (let i = smallerString.length; i > 0; i--) {
+        const testString = smallerString.slice(0, i);
+        const correctSmaller = !smallerString.split(testString).join('').length;
+        const correctLarger = !largerString.split(testString).join('').length;
+        if (correctSmaller && correctLarger) return testString;
     }
-    
-    return ans 
+    return '';
 };
