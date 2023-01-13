d4977c2c01b9529ad39d2c6c28a3d6af5aeab210
이시형 <tlgud585@gmail.com>
2023-01-14 03:24:15 +0900

    Time: 101 ms (67.80%), Space: 49.9 MB (34.38%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.js
+++ 0200-number-of-islands/0200-number-of-islands.js
@@ -3,29 +3,24 @@
  * @return {number}
  */
 var numIslands = function(grid) {
-    const isValid = (x,y) => {
-        return x>=0 && y>=0 && x<grid.length && y< grid[0].length && grid[x][y]==="1"
-    }
-    // function DFS(x,y){
-    //     if(isValid(x,y)){
-    //         grid[x][y] = 0
-    //         return DFS(x-1,y) ||  DFS(x,y+1) || DFS(x+1,y) || DFS(x,y-1)
-    //     }
-    // }
-    // let count = 0 
-    // for(let i=0; i<grid.length; i++){
-    //     for(let j=0; j<grid[0].length; j++){
-    //         if(grid[i][j] === "1"){
-    //             count++
-    //             DFS(i,j)
-    //         }
-    //     }
-    // }
-    // return count
-    
     let count = 0 
 
     const directions = [[-1,0],[0,1],[1,0],[0,-1]]
+    
+    const isValid = (x,y) => {
+        return x>=0 && y>=0 && x<grid.length && y< grid[0].length && grid[x][y]==="1"
+    }
+    
+    function DFS(x,y){
+        if(isValid(x,y)){
+            grid[x][y] = 0
+            // return DFS(x-1,y) ||  DFS(x,y+1) || DFS(x+1,y) || DFS(x,y-1)
+            directions.forEach((d)=> DFS(x+d[0],y+d[1]))
+        }
+    }
+
+    
+    
     
     const BFS = (i,j) => {
         let queue = [[i,j]]
@@ -52,8 +47,7 @@ var numIslands = function(grid) {
         for(let j=0; j<grid[0].length; j++){
             if(grid[i][j] === "1"){
                 count++
-                // grid[i][j] = '0'
-                BFS(i,j)
+                DFS(i,j)
             }
         }
     }
