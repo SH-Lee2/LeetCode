d51be31a8a28abf88df13d67d0b7070ba9b812a3
이시형 <tlgud585@gmail.com>
2023-01-31 23:53:03 +0900

    Time: 93 ms (85.71%), Space: 55 MB (30.95%) - LeetHub

--- 2091-removing-minimum-and-maximum-from-array/2091-removing-minimum-and-maximum-from-array.js
+++ 2091-removing-minimum-and-maximum-from-array/2091-removing-minimum-and-maximum-from-array.js
@@ -12,13 +12,15 @@ var minimumDeletions = function(nums) {
     const right = Math.max(maximumIdx, minimumIdx)
     
     // case 1 : 앞에서 부터 삭제
-    console.log(right+1)
-    // case 2 : 뒤에서 부터 삭제
-    console.log(nums.length - left)
-    // case 3 : 앞 뒤에서 부터 삭제  
-    console.log(left + 1 + nums.length - right)
+    const case1 = right + 1
     
-    return Math.min(right + 1, nums.length - left, left + 1 + nums.length - right)
+    // case 2 : 뒤에서 부터 삭제
+    const case2 = nums.length - left
+    
+    // case 3 : 앞 뒤에서 부터 삭제  
+    const case3 = (left + 1) + (nums.length - right)
+    
+    return Math.min(case1, case2, case3)
 };
 
 function findMaximumIdx(nums) {
