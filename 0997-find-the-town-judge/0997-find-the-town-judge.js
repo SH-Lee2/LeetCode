ed4eef7d3a3797b0470ce84b3fadf5c5ad7d100e
이시형 <tlgud585@gmail.com>
2023-02-01 14:05:03 +0900

    Time: 89 ms (97.95%), Space: 51 MB (35.46%) - LeetHub

--- 0997-find-the-town-judge/0997-find-the-town-judge.js
+++ 0997-find-the-town-judge/0997-find-the-town-judge.js
@@ -4,33 +4,16 @@
  * @return {number}
  */
 var findJudge = function(n, trust) {
-    // 방향 그래프 , [1,2] 1는 2를 믿지만 2는 1일 믿지 않는다 즉 2는 판사! 
+    const counts = new Array(n+1).fill(0)
     
-    // 판사 조건1. 아무도 믿지 않는다.
-    // 판사 조건2. 모두 자기를 믿는다.
-    let ans = -1
-    
-    const people = new Map()
-    const count = new Map()
+    for(const [i,j] of trust){
+        counts[i] -= 1
+        counts[j] += 1
+    }
     
     for(let i=1; i<=n; i++){
-        people.set(i,[])
-    }
-     for(let i=1; i<=n; i++){
-        count.set(i,0)
+        if(counts[i] === n-1) return i 
     }
     
-    trust.forEach(p=> {
-        people.get(p[0]).push(p[1])
-        count.set(p[1], (count.get(p[1])||0)+1)
-    })
-    
-    for(const [key, value] of people){
-        if(value.length === 0){
-            if(count.get(key) ===  n-1) return key
-            else return -1
-        }
-    }
-    
-    return ans
+    return -1
 };
