d6c4e34d1f539c38c4bded48e4f71403d5e58c84
이시형 <tlgud585@gmail.com>
2023-02-02 14:43:45 +0900

    Time: 73 ms (50.77%), Space: 42.5 MB (65.05%) - LeetHub

--- 0953-verifying-an-alien-dictionary/0953-verifying-an-alien-dictionary.js
+++ 0953-verifying-an-alien-dictionary/0953-verifying-an-alien-dictionary.js
@@ -1,15 +1,23 @@
-var isAlienSorted = function(W, O) {
-    let alpha = new Map([["",-1]])
-    for (let i = 0; i < O.length; i++)
-        alpha.set(O.charAt(i), i)
-    for (let i = 1; i < W.length; i++) {
-        let a = W[i-1], b = W[i]
-        for (let j = 0; j < a.length; j++) {
-            let achar = a.charAt(j), bchar = b.charAt(j),
-                aix = alpha.get(achar), bix = alpha.get(bchar)
-            if (aix < bix) break
-            if (aix > bix) return false
+/**
+ * @param {string[]} words
+ * @param {string} order
+ * @return {boolean}
+ */
+var isAlienSorted = function(words, order) {
+    const dic = new Map([["",-1]])
+    
+    for(let i=0; i<order.length; i++){
+        dic.set(order[i], i+1)
+    }
+    
+    for(let i=1; i<words.length; i++){
+        const a = words[i-1], b = words[i] 
+        for(let j=0; j<a.length; j++){
+            const aChar = a.charAt(j), bChar = b.charAt(j)
+            if(dic.get(aChar) < dic.get(bChar)) break
+            if(dic.get(aChar) > dic.get(bChar)) return false
         }
     }
+        
     return true
 };
