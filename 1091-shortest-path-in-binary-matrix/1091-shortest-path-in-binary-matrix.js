12605266de23a328e538cdef036ba5922e454811
이시형 <tlgud585@gmail.com>
2023-02-06 13:54:39 +0900

    Time: 117 ms (77.22%), Space: 49.2 MB (74.85%) - LeetHub

--- 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.js
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.js
@@ -3,45 +3,39 @@
  * @return {number}
  */
 var shortestPathBinaryMatrix = function(grid) {
-    // 이동 방향 위 아래 오른쪽 왼쪽 대각선
-    const directions = [[0, 1], [1, 0], [1, 1], [-1, -1], [-1, 1], [1, -1], [0, -1], [-1, 0]];
-
+    if(grid[0][0] === 1) return -1
+    
     const len = grid.length
+    const directions = [[-1,0],[-1,1], [0,1], [1,1], [1,0], [1,-1],[0,-1],[-1,-1]]
+    const seen = Array.from(Array(len), ()=>Array(len).fill(false))
+    seen[0][0] = true
+    let queue = [[0,0]]
+    let steps = 0
     
-    if (grid[0][0] == 1) {
-        return -1;
-    }
-    
-    let queue = [[0,0,1]]
-    grid[0][0] = -1
-    
-    const isValid = (x,y) => {
+    const gridIsValid = (x,y) => {
         return x >= 0 && x < len && y >= 0 && y < len && grid[x][y] === 0
     }
     
-    const isEnd = (x,y) => {
-        return x === len-1 && y === len-1    
-    }
-    
     while(queue.length){
-        const currentLength = queue.length
-        const nextQueue = []
-        for(let i=0; i<currentLength; i++){
-            let [x, y, distance] = queue[i]
-            if(isEnd(x,y)) return distance
-            distance++
-
-            for(const [dx, dy] of directions){
-                const nx = x + dx
-                const ny = y + dy
-                if(isValid(nx,ny)){
-                    grid[nx][ny] = -1
-                    nextQueue.push([nx,ny,distance])
+        const nextQueue = [] 
+        steps++
+        
+        for(const [x,y] of queue){
+            if(x === len-1 && y === len-1) return steps
+            
+            for(const [dx,dy] of directions){
+                const nx = x+dx, ny = y+dy
+                
+                if(gridIsValid(nx,ny) && seen[nx][ny] === false){
+                    seen[nx][ny] = true
+                    nextQueue.push([nx,ny])
+                    
                 }
             }
         }
+        
         queue = nextQueue
     }
     
-    return -1
+    return -1 
 };
