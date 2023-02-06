386ea76a2b3ec5dae45bfd02b4082ef84ff9923f
이시형 <tlgud585@gmail.com>
2023-02-06 15:36:46 +0900

    Time: 164 ms (80.14%), Space: 50.7 MB (72.03%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.js
+++ 0542-01-matrix/0542-01-matrix.js
@@ -3,48 +3,43 @@
  * @return {number[][]}
  */
 var updateMatrix = function(mat) {
-    const isValid = (x, y) => {
-        return 0 <= x && x < rows && 0 <= y && y < cols && mat[x][y] == 1;
+    const gridIsValid = (x,y) => {
+        return x >= 0 && x < m && y >= 0 && y < n && mat[x][y] === 1
     }
+
+    const m = mat.length, n = mat[0].length
+    let queue = [] 
+    let seen = Array.from({length:m}, () => Array(n).fill(false))
     
-    const rows = mat.length;
-    const cols = mat[0].length;
-    let queue = [];
-    const visited = Array.from(Array(rows),()=>Array(cols).fill(0))
-    
-    for (let x = 0; x < rows; x++) {
-        for (let y = 0; y < cols; y++) {
-            if (mat[x][y] == 0) {
-                queue.push([x, y]);
+    for(let row=0; row<m; row++){
+        for(let col=0; col<n; col++){
+            if(mat[row][col] === 0){
+                queue.push([row,col])
+                seen[row][col] = true
             }
         }
-    }
+    }   
     
-    let directions = [[0, 1], [1, 0], [0, -1], [-1, 0]];
-    let steps = 0;
+    const directions = [[-1,0],[0,1],[1,0],[0,-1]]  
+    let steps = 0
     
-    while (queue.length) {
-        let currentLength = queue.length;
-        let nextQueue = [];
-        steps++;
-        // 그래프에서 level마다 모든 노드를 확인하는것과 동일
-        for (let i = 0; i < currentLength; i++) {
-            // shift보다 성능이 좋다, shift할 경우 배열의 인덱스 번호를 다시 재정렬해줘야함
-            const [x, y] = queue[i]; 
-            visited[x][y] = true;
-
-            for (const [dx, dy] of directions) {
-                let nx = x + dy, ny = y + dx;
-                if (isValid(nx, ny) && !visited[nx][ny]) {
-                    visited[nx][ny] = true;
-                    nextQueue.push([nx, ny]);
-                    mat[nx][ny] = steps;
+    while(queue.length){
+        const nextQueue = []
+        steps++
+        
+        for(const [row,col] of queue){
+            for(const [dx,dy] of directions){
+                const nextRow = row + dx, nextCol = col + dy
+                if(gridIsValid(nextRow,nextCol) && !seen[nextRow][nextCol]){
+                    seen[nextRow][nextCol] = true
+                    nextQueue.push([nextRow,nextCol])
+                    mat[nextRow][nextCol] = steps
                 }
             }
         }
         
-        queue = nextQueue;
+        queue = nextQueue
     }
     
-    return mat;
-};
+    return mat
+}
