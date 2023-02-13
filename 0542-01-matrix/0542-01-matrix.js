7193df4a3059685911c94ace64c9d99edda511e6
이시형 <tlgud585@gmail.com>
2023-02-14 03:28:01 +0900

    Time: 179 ms (64.48%), Space: 51.6 MB (62.91%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.js
+++ 0542-01-matrix/0542-01-matrix.js
@@ -3,37 +3,35 @@
  * @return {number[][]}
  */
 var updateMatrix = function(mat) {
+    let queue = []
+    const m=mat.length, n=mat[0].length
+    const directions = [[-1,0],[0,1],[1,0],[0,-1]]
+    const visited = Array.from({length:m},()=>Array(n).fill(false))
+    
     const gridIsValid = (x,y) => {
-        return x >= 0 && x < m && y >= 0 && y < n && mat[x][y] === 1
+        return x>=0 && x<m && y>=0 && y<n && mat[x][y] === 1
     }
-
-    const m = mat.length, n = mat[0].length
-    let queue = [] 
-    let seen = Array.from({length:m}, () => Array(n).fill(false))
     
     for(let row=0; row<m; row++){
         for(let col=0; col<n; col++){
             if(mat[row][col] === 0){
-                queue.push([row,col])
-                seen[row][col] = true
+                queue.push([row,col,0])
+                visited[row][col] = true
             }
         }
-    }   
-    
-    const directions = [[-1,0],[0,1],[1,0],[0,-1]]  
-    let steps = 0
+    }
     
     while(queue.length){
-        const nextQueue = []
-        steps++
+        const nextQueue = [] 
         
-        for(const [row,col] of queue){
+        for(const [x,y,distance] of queue){
             for(const [dx,dy] of directions){
-                const nextRow = row + dx, nextCol = col + dy
-                if(gridIsValid(nextRow,nextCol) && !seen[nextRow][nextCol]){
-                    seen[nextRow][nextCol] = true
-                    nextQueue.push([nextRow,nextCol])
-                    mat[nextRow][nextCol] = steps
+                const nx=x+dx, ny=y+dy
+                
+                if(gridIsValid(nx,ny) && !visited[nx][ny]){
+                    mat[nx][ny] = distance+1
+                    visited[nx][ny] = true
+                    nextQueue.push([nx,ny,distance+1])
                 }
             }
         }
