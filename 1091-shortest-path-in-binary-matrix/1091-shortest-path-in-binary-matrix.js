eecfe0c4f8c9ac0c9d3d520d7792eb6bd94c1a72
이시형 <tlgud585@gmail.com>
2023-02-14 04:38:50 +0900

    Time: 104 ms (86.89%), Space: 49.3 MB (62.08%) - LeetHub

--- 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.js
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.js
@@ -3,39 +3,42 @@
  * @return {number}
  */
 var shortestPathBinaryMatrix = function(grid) {
-    if(grid[0][0] === 1) return -1
+    // 초기 스타트 지점이 1이라면 -1 리턴
+    if(grid[0][0]) return -1
     
-    const len = grid.length
-    const directions = [[-1,0],[-1,1], [0,1], [1,1], [1,0], [1,-1],[0,-1],[-1,-1]]
-    const seen = Array.from(Array(len), ()=>Array(len).fill(false))
-    seen[0][0] = true
-    let queue = [[0,0]]
-    let steps = 0
+    const n = grid.length,
+          directions=[[-1,0],[-1,1],[0,1],[1,1],[1,0],[1,-1],[0,-1],[-1,-1]]
     
     const gridIsValid = (x,y) => {
-        return x >= 0 && x < len && y >= 0 && y < len && grid[x][y] === 0
+        return x>=0 && x<n && y>=0 && y<n && grid[x][y] === 0
     }
     
+    let queue = [[0,0]] 
+    grid[0][0] = 1
+    
+    let step = 0
     while(queue.length){
-        const nextQueue = [] 
-        steps++
+        const nextQueue = []
+        step++
         
         for(const [x,y] of queue){
-            if(x === len-1 && y === len-1) return steps
             
+            if(x === n-1 && y === n-1) return step
+
             for(const [dx,dy] of directions){
-                const nx = x+dx, ny = y+dy
+                const nx=x+dx, ny=y+dy
                 
-                if(gridIsValid(nx,ny) && seen[nx][ny] === false){
-                    seen[nx][ny] = true
+                if(gridIsValid(nx,ny)){
                     nextQueue.push([nx,ny])
-                    
+                    grid[nx][ny] = 1
+
                 }
+                
             }
         }
         
-        queue = nextQueue
+        queue=nextQueue
     }
     
-    return -1 
+    return -1
 };
