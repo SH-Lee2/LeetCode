f30d8ef51e6b42aaad7caddfcfe5a92072c7db45
이시형 <tlgud585@gmail.com>
2023-02-21 14:25:20 +0900

    Time: 60 ms (88.33%), Space: 44.4 MB (96.50%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.js
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.js
@@ -3,8 +3,12 @@
  * @return {number}
  */
 var singleNonDuplicate = function(nums) {
-    for(let i=0; i<nums.length; i++){
-        if(nums[i] === nums[i+1]) i++
-        else return nums[i]
+    let left = 0, right = nums.length - 1;
+    while (left < right) {
+        let mid = (left + right) >> 1
+        if (mid % 2 === 1) mid--; // 중간 지점을 항상 짝수 인덱스로 만듦
+        if (nums[mid] === nums[mid + 1]) left = mid + 2; 
+        else right = mid-1;
     }
+    return nums[left];
 };
