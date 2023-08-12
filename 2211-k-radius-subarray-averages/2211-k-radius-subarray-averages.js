6620cb2adc6dcb02fe82dcfd4684f1787820ee11
이시형 <tlgud585@gmail.com>
2023-08-12 14:50:58 +0900

    Time: 324 ms (96.39%), Space: 89.9 MB (34.74%) - LeetHub

---
+++ 2211-k-radius-subarray-averages/2211-k-radius-subarray-averages.js
@@ -0,0 +1,28 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number[]}
+ */
+var getAverages = function(nums, k) {
+    if(k===0) return nums 
+
+    // sliding window + prefix sum ?? 
+    const len = nums.length
+    const ans = Array.from({length: len},()=>-1), prefix = [0, nums[0]]
+
+    for(let i=1; i<len; i++){
+        prefix.push(nums[i] + prefix[i])
+    }
+    const divisor = k*2+1
+
+    for(let i=0; i<len; i++){
+        if(i < k || i+k > len-1) {
+            continue
+        }
+        
+        const avg = Math.floor((prefix[i+k+1] - prefix[i-k])/divisor)
+        ans[i] = avg
+    }
+    return ans
+}
+
