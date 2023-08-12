2c8c73f7cff26b95810381525a11ec8c761b894b
이시형 <tlgud585@gmail.com>
2023-08-12 13:32:47 +0900

    Time: 48 ms (93.9%), Space: 42.4 MB (50.18%) - LeetHub

---
+++ 1603-running-sum-of-1d-array/1603-running-sum-of-1d-array.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var runningSum = function(nums) {
+    const ans = [nums[0]]
+
+    for(let i=1; i<nums.length; i++){
+        ans.push(nums[i] + ans[i-1])
+    }
+
+    return ans
+};
