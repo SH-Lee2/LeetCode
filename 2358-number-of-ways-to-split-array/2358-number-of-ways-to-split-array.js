18131f5de669519d8485686022283e308d903d9f
이시형 <tlgud585@gmail.com>
2023-08-12 12:18:04 +0900

    Time: 115 ms (27.62%), Space: 59.2 MB (61.9%) - LeetHub

--- 2358-number-of-ways-to-split-array/2358-number-of-ways-to-split-array.js
+++ 2358-number-of-ways-to-split-array/2358-number-of-ways-to-split-array.js
@@ -3,18 +3,34 @@
  * @return {number}
  */
 var waysToSplitArray = function(nums) {
-    const prefix = [nums[0]]
+    // const prefix = [nums[0]]
     
-    // prefix 구하기 
-    for(let i=1; i<nums.length; i++){
-        prefix.push(nums[i] + prefix[i-1])
-    }
+    // // prefix 구하기 
+    // for(let i=1; i<nums.length; i++){
+    //     prefix.push(nums[i] + prefix[i-1])
+    // }
     
+    // let ans = 0 
+    // for(let i=0; i<nums.length-1; i++){
+    //     const left = prefix[i]
+    //     const right = prefix[nums.length - 1] - left
+    //     if(left >= right) ans++
+    // }
+    
+    // return ans
+
+    // prefix array는 필요 없다.
+
+    let total = 0, leftSum = 0
+    nums.map(num => total += num)
+
     let ans = 0 
+
     for(let i=0; i<nums.length-1; i++){
-        const left = prefix[i]
-        const right = prefix[nums.length - 1] - left
-        if(left >= right) ans++
+        leftSum += nums[i]
+        const rightSum = total - leftSum
+
+        if(leftSum >= rightSum) ans++
     }
     
     return ans
