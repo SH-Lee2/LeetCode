8b4193dd875efd45193056bf6c5366cf6e87e31b
이시형 <tlgud585@gmail.com>
2023-08-13 14:32:51 +0900

    Time: 63 ms (71.56%), Space: 46 MB (60.39%) - LeetHub

--- 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.js
+++ 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.js
@@ -4,15 +4,16 @@
  * @return {number}
  */
 var minSubArrayLen = function(target, nums) {
-    let left = 0, ans = Infinity, sum = 0;
+    let left = 0, ans = Infinity, curr = 0
 
-    for (let right = 0; right < nums.length; right++) {
-        sum+= nums[right]
-        while (sum >= target) {
-            ans = Math.min(ans, right+1-left)
-            sum -= nums[left++]
+    for(let right=0; right<nums.length; right++){
+        curr += nums[right]
+
+        while(curr >= target){
+            ans = Math.min (ans, right-left+1)
+            curr -= nums[left++]
         }
     }
 
-    return ans === Infinity ? 0 : ans;
+    return ans === Infinity ? 0 : ans
 };
