26006e6ef2ab13e29765564fd93d5149652e63fd
이시형 <tlgud585@gmail.com>
2023-08-17 23:59:43 +0900

    Time: 71 ms (92.05%), Space: 49.8 MB (93.23%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.js
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.js
@@ -4,16 +4,15 @@
  * @return {number}
  */
 var subarraySum = function(nums, k) {
-    let ans = 0 
-    const map = new Map()
-    map.set(0,1)
-    
-    let curr = 0
-    for(const num of nums){
-        curr+= num 
-        ans += (map.get(curr-k)||0)
-        map.set(curr,(map.get(curr) || 0) + 1)
+    let counts = new Map();
+    counts.set(0, 1);
+    let ans = 0, curr = 0;
+
+    for (const num of nums) {
+        curr += num;
+        ans += counts.get(curr - k) || 0;
+        counts.set(curr, (counts.get(curr) || 0) + 1);
     }
-    
-    return ans
+
+    return ans;
 };
