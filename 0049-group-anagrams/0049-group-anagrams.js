d4037704bb7bd6879523e855d88b466a406ab0b7
이시형 <tlgud585@gmail.com>
2023-08-20 13:49:30 +0900

    Time: 130 ms (44.78%), Space: 56.6 MB (11.27%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.js
+++ 0049-group-anagrams/0049-group-anagrams.js
@@ -3,18 +3,28 @@
  * @return {string[][]}
  */
 var groupAnagrams = function(strs) {
-    const map = new Map()
-    
-    strs.forEach(str=>{
-        const key = str.split('').sort().join('')
-        if(!map.has(key)) map.set(key,[])
-        map.get(key).push(str)
-    })
-    
-    const ans = [] 
-    for(const values of map.values()){
-        ans.push(values)
+    // 같은 아나그램 문자끼리 그룹화 하여 리턴하는 문제
+
+    const dic = new Map()
+
+    for(let str of strs){
+        //각 문자열을 단어 단위로 split 
+        const chars = str.split('')
+        
+        //split한 문자열을 오름차순 정렬하고 그것을 key 값으로 value는 기존 문자열을 저장
+        const key = chars.sort((a,b)=>a.localeCompare(b)).join('')
+        
+        if(dic.has(key)){
+            dic.get(key).push(str)
+        }else{
+            dic.set(key, [str])
+        }
     }
-    
+
+    const ans = [] 
+    for(const value of dic.values()){
+        ans.push(value)
+    }
+
     return ans
 };
