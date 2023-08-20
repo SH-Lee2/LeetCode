fdaa86a00322f5cf43c1b18d39e05da3a9eec4b3
이시형 <tlgud585@gmail.com>
2023-08-20 14:32:38 +0900

    Time: 1357 ms (5.31%), Space: 82.9 MB (28.32%) - LeetHub

---
+++ 2473-max-sum-of-a-pair-with-equal-sum-of-digits/2473-max-sum-of-a-pair-with-equal-sum-of-digits.js
@@ -0,0 +1,26 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var maximumSum = function(nums) {
+    // 숫자 num의 각 자리수의 합이 같은것 끼리의 합이 가장 큰 합 찾는 문제 
+    // 49 , 76 => 13 === 13 => 125
+
+    // 각 숫자의 자리수 합을 고유 키로 사용
+    const dic = new Map()
+    let ans = -1
+
+    for(const num of nums){
+        const key = num.toString().split('').reduce((a,b)=> +a + +b,'')
+        
+        if(!dic.has(key)) dic.set(key, [num])
+        else {
+            // key가 있는 경우 value, ans 업데이트
+            const values = dic.get(key)
+            values.forEach(value => ans = Math.max(ans, value + num))
+            values.push(num)
+        }
+    }
+    
+    return ans 
+};
