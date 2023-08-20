134a413598dd5ca1ff6f29af387e44725d88f973
이시형 <tlgud585@gmail.com>
2023-08-20 14:18:06 +0900

    Time: 115 ms (80.92%), Space: 75.5 MB (95.42%) - LeetHub

--- 2338-minimum-consecutive-cards-to-pick-up/2338-minimum-consecutive-cards-to-pick-up.js
+++ 2338-minimum-consecutive-cards-to-pick-up/2338-minimum-consecutive-cards-to-pick-up.js
@@ -3,22 +3,20 @@
  * @return {number}
  */
 var minimumCardPickup = function(cards) {
-    if(new Set(cards).size === cards.length) return -1
-
     const dic = new Map()
     let ans = Infinity
 
     for(let i=0; i<cards.length; i++){
         const card = cards[i]
         
-        // dic에 card 정보가 없는 경우
-        if(!dic.has(card)) dic.set(card,i)
-        else{
+        // dic에 card 정보가 있는 경우
+        if(dic.has(card)){
             // card 정보가 있는 경우 value, ans 업데이트 
             const value = dic.get(card)
             ans = Math.min(ans,i - value + 1)
-            dic.set(card, i) // 다음 차례에 같은 숫자가 나오더라도 최소 길이를 찾는거기 때문에 현재 인덱스로 변경
+            dic.set(card, i) 
         }
+        dic.set(card,i)
     }
 
     return ans === Infinity ? -1 : ans
