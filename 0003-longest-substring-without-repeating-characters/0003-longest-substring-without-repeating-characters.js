124af8e06d7ab46f0c7057751c171134eb621a51
이시형 <tlgud585@gmail.com>
2023-08-21 22:39:06 +0900

    Time: 64 ms (95.21%), Space: 44.5 MB (96.82%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.js
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.js
@@ -3,18 +3,18 @@
  * @return {number}
  */
 var lengthOfLongestSubstring = function(s) {
-    const map = new Map()
-    let ans = 0 //aab
-    let left = 0
+    const dic = new Map() 
+    let ans = 0, left = 0
 
-    for(let i=1; i<=s.length; i++){
-        const char = s[i-1]
-        if(map.has(char)){
-            left = Math.max(map.get(char), left)
+    for(let i=0; i<s.length; i++){
+        const char = s[i] 
+
+        if(dic.has(char)){
+            left = Math.max(dic.get(char), left)
         }
-        ans = Math.max(ans, i - left) //0
-        map.set(char, i)
+        ans = Math.max(ans, i - left  + 1)
+        dic.set(char, i+1)
     }
-    
+
     return ans
 };
