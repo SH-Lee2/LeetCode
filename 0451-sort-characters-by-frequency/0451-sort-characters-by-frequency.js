0940b0ff5513492eced466f74731a3d760e27ab4
이시형 <tlgud585@gmail.com>
2023-09-25 08:15:17 +0900

    Time: 66 ms (81.66%), Space: 46.4 MB (61.6%) - LeetHub

--- 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.js
+++ 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.js
@@ -3,10 +3,11 @@
  * @return {string}
  */
 var frequencySort = function(s) {
-    const map = new Map()
-    
-    s.split('').forEach(char => map.set(char, (map.get(char)||0)+1))
-    
-    return [...map].sort((a,b)=> b[1] - a[1]).map((item) => item[0].repeat(item[1])).join('')
-    
+    const frequencyMap = new Map()
+
+    s.split('').forEach(char=>frequencyMap.set(char,(frequencyMap.get(char)||0)+1))
+
+    const sortFrequency = [...frequencyMap.entries()].sort((a,b)=> b[1] - a[1])
+
+    return sortFrequency.map(([char, count])=>char.repeat(count)).join('')
 };
