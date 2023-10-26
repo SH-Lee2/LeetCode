556e62e4df623bbb8fc83dec0bb2ebd0cf4de9a7
이시형 <tlgud585@gmail.com>
2023-10-26 12:41:05 +0900

    Time: 62 ms (88.74%), Space: 45.1 MB (58.49%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.js
+++ 0567-permutation-in-string/0567-permutation-in-string.js
@@ -4,29 +4,32 @@
  * @return {boolean}
  */
 var checkInclusion = function(s1, s2) {
-    if (s1.length > s2.length) return false;
+    if(s1.length > s2.length) return false 
+
     const map = new Map()
+    s1.split("").forEach(s=>map.set(s, (map.get(s)||0)+1))
 
-    s1.split('').forEach((s)=>map.set(s, (map.get(s)||0)+1))
+    let left = 0, 
+		right = 0, 
+		requiredLength = s1.length
 
-    for(let i=0; i<=s2.length - s1.length; i++){
-         const track = new Map()
-         for(let j=0; j<s1.length; j++){
-             const trackKey = s2[i+j]
+		while (right < s2.length) {
+		if (map.get(s2[right]) > 0) {
+			requiredLength--
+		}
+		map.set(s2[right], map.get(s2[right])-1)
 
-             // 현재 단어가 map에 없는 경우 for문으로 넘어감
-             if(!map.has(trackKey)) break;
+		right++ 
 
-             // 현재 단어를 track 에 저장 
-             track.set(trackKey,(track.get(trackKey)||0)+1)
+		if (requiredLength === 0) return true;
 
-            // track 값이 더 큰 경우다음 for문으로 넘어감
-             if(track.get(trackKey) > map.get(trackKey)) break
-
-             // j의 값이 s1의 길이와 같은 경우 s1의 길이만큼의 순열을 가지고 있기 때문에 true 리턴
-             if(j+1 === s1.length) return true
-         }
-    }
-
-    return false
+		if (right - left === s1.length) {
+			if (map.get(s2[left]) >= 0) {
+				requiredLength++;
+			}
+        map.set(s2[left], map.get(s2[left])+1)
+        left++
+		}
+	}
+	return false;
 };
