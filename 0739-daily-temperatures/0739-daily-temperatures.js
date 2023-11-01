6118db3db8ef86c6b23941883b318c88e2137741
이시형 <tlgud585@gmail.com>
2023-11-02 08:20:21 +0900

    Time: 307 ms (29.03%), Space: 66 MB (63.62%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.js
+++ 0739-daily-temperatures/0739-daily-temperatures.js
@@ -3,17 +3,17 @@
  * @return {number[]}
  */
 var dailyTemperatures = function(temperatures) {
-    // 모노토닉
-    const stack = [] 
-    const ans = new Array(temperatures.length).fill(0)
-    
+    const stack = []
+    const answer = Array.from({length : temperatures.length}, () => 0)
+
     for(let i=0; i<temperatures.length; i++){
-        while(stack.length && temperatures[stack[stack.length-1]] < temperatures[i]){
-            const day = stack.pop()
-            ans[day] = i - day 
+        while(stack.length && temperatures[stack.at(-1)] < temperatures[i]){
+            const j = stack.pop()
+            answer[j] = i - j
         }
+
         stack.push(i)
     }
-    
-    return ans
+
+    return answer
 };
