ba53021d713ee2469b6290091485deefb0d4b2a9
이시형 <tlgud585@gmail.com>
2023-11-07 13:36:35 +0900

    Time: 731 ms (26.54%), Space: 90.9 MB (58.62%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.js
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.js
@@ -3,34 +3,30 @@
  * @param {number} k
  * @return {number[]}
  */
-
-// monotonic
 var maxSlidingWindow = function(nums, k) {
-    const queue = [] 
-    const answer = []
-    
-    for(let i=0; i<nums.length; i++){
-        while(queue && nums[queue[queue.length-1]] < nums[i]){
+    let ans = [];
+    let queue = [];
+
+    // k 길이만큼의 window 에서만 최대 값을 찾는게 중요
+    // window 내부에서는 내림차순 정렬
+    for (let i = 0; i < nums.length; i++) {
+        // 현재 값 보다 이전 값들이 작은 경우 제거 dequeue
+        while(queue.length && nums[i] > nums[queue.at(-1)]){
             queue.pop()
         }
-        
-        queue.push(i)
-        
-        /**
-        * 만약 nums 배열이 [1,3,-1,-3,5,4,3,2] 라면
-        * queue에 [5,4,3,2] 가 남는다 이렇게 되면 k(3)을 넘게 되므로 5를 빼준다.
-        * 즉 위의 while문 조건에 걸리지 않고 k 이상일때 넘지 않도록 제거해주는 로직이다.
-        */
-       if (queue[0] + k === i) {
-             queue.shift();
+
+        // 윈도우 사이즈 계산 때문에 인덱스 추가
+        queue.push(i);
+
+        // queue 의 길이가 window 사이즈 초과 인경우 제거
+        if(queue[0] + k === i){
+            queue.shift()
         }
-        
-        // 처음 k개 이후에는 매번 answer에 push
-        if(i >= k - 1) {
-            answer.push(nums[queue[0]])
+
+        if(i >= k - 1){
+            ans.push(nums[queue[0]])
         }
     }
     
-    
-    return answer
+    return ans;
 };
