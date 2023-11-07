1fac6229618c877e8dfe0185c60a916de6c5cad6
이시형 <tlgud585@gmail.com>
2023-11-07 17:56:44 +0900

    Time: 77 ms (91.25%), Space: 55.6 MB (22.5%) - LeetHub

--- 1549-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit/1549-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit.js
+++ 1549-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit/1549-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit.js
@@ -4,31 +4,39 @@
  * @return {number}
  */
 var longestSubarray = function(nums, limit) {
-    const increaseArr = []
-    const decreaseArr = []
-    let ans = 0, left = 0 
+    
+    let sizeResult = 0;
 
-    for(let right=0; right<nums.length; right++){
-        while(increaseArr.length && increaseArr.at(-1) > nums[right]){
-            increaseArr.pop()
+    let queueMin = [];
+    let queueMax = [];
+
+    for (let left = 0, right = 0; right < nums.length; right++) {
+
+        while (queueMin.length && nums[right] < nums[queueMin[queueMin.length - 1]]) {
+            queueMin.pop();
+        }
+        queueMin.push(right);
+
+        while (queueMax.length && nums[right] > nums[queueMax[queueMax.length - 1]]) {
+            queueMax.pop();
+        }
+        queueMax.push(right);
+
+        if (nums[queueMax[0]] - nums[queueMin[0]] > limit) {
+            
+            if (queueMin[0] == left) {
+                queueMin.shift()
+            }
+
+            if (queueMax[0] == left) {
+                queueMax.shift()
+            }
+
+            left++;
         }
 
-        while(decreaseArr.length && decreaseArr.at(-1) < nums[right]){
-            decreaseArr.pop()
-        }
-
-        increaseArr.push(nums[right])
-        decreaseArr.push(nums[right])
-
-        while((decreaseArr[0] - increaseArr[0]) > limit){
-            if(nums[left] === decreaseArr[0]) decreaseArr.shift()
-            if(nums[left] === increaseArr[0]) increaseArr.shift()
-
-            left++
-        }
-
-        ans = Math.max(ans, right-left+1)
+        sizeResult = Math.max(sizeResult, right - left + 1);
     }
 
-    return ans
+    return sizeResult;
 };
