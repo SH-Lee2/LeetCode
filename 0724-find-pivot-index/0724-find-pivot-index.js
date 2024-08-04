657cda9bafedb343d9e079cf092d3840b0df1996
이시형 <tlgud585@gmail.com>
2024-08-04 16:29:23 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0724-find-pivot-index/0724-find-pivot-index.js
+++ 0724-find-pivot-index/0724-find-pivot-index.js
@@ -3,14 +3,16 @@
  * @return {number}
  */
 var pivotIndex = function(nums) {
-    // total 값을 구한다.
-    let total = nums.reduce((a,b)=> a+b,0), leftSum = 0 
+    let ans = -1 // init pivot index
+    let right = 0 , left = 0  
 
-    // 왼쪽부터 값을 차례대로 제거하고 왼쪽 값을 더한 값과 total 값을 비교한다.
-    for(let left=0; left<nums.length; left++){
-        if(leftSum === total - leftSum - nums[left]) return left
-        leftSum += nums[left]
+    for(let i=1;i<nums.length; i++) right += nums[i]
+
+    for(let i=0; i<nums.length-1; i++){
+        if(left === right) return i
+        left += nums[i]
+        right -= nums[i+1]
     }
 
-    return -1
+    return ans
 };
